0b49352bb37a54e65f0f1565019f28531549c47b
DLinh <nogamenolife.why@gmail.com>
2022-05-20 22:16:46 +0700

    refactor: migrated categories, cart to typescript

--- src/store/cart/cart.selector.ts
+++ src/store/cart/cart.selector.ts
@@ -1,6 +1,7 @@
+import { CartState } from './cart.reducer';
 import { createSelector } from 'reselect';
 
-const selectCartReducer = (state) => state.cart;
+const selectCartReducer = (state: any) : CartState => state.cart;
 
 export const selectIsCartOpen = createSelector(
   [selectCartReducer],
